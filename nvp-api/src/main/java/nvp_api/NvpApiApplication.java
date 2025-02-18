5fec7fa13dd19ed09310e4ca7c1a6d7ec1e3580e
PBEM22 <dlacofbs1229@gmail.com>
2025-02-18 19:04:48 +0900

    feat: #1 초기 환경 세팅 - 의존성 주입

---
+++ nvp-api/src/main/java/nvp_api/NvpApiApplication.java
@@ -0,0 +1,13 @@
+package nvp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvpApiApplication.class, args);
+	}
+
+}
---
+++ nvp-api/src/test/java/nvp_api/NvpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nvp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
