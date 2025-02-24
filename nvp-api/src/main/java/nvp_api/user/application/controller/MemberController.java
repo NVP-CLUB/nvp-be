aa2750fe170f7366c588373fd3b90b403160b293
PBEM22 <dlacofbs1229@gmail.com>
2025-02-24 23:14:53 +0900

    feat: #7 middle save

---
+++ nvp-api/src/main/java/nvp_api/user/application/controller/MemberController.java
@@ -0,0 +1,13 @@
+package nvp_api.user.application.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class MemberController {
+
+
+}
